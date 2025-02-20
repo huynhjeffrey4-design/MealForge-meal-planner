5f9487487c170291f683f50d08dfb2f3a5fdb76e
jhuynh34buffalo <jhuynh3@buffalo.edu>
2025-02-20 14:32:20 -0500

    feat: Add secure registration form

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'jhuynh3');       // Your UBITName (e.g., jhuynh3)
+define('DB_PASS', '50460153');        // 8-digit person number
+define('DB_NAME', 'cse442_2025_spring_team_v_db');
+   ?>
