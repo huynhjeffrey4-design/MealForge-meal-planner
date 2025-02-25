1bc00cdf54464d14ce9998d6cc844f37b49a6ed5
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-02-25 17:50:42 -0500

    chore: formatting

--- dashboard.php
+++ dashboard.php
@@ -2,9 +2,8 @@
 session_start();
 
 if (!isset($_SESSION['user_id']) &&  !isset($_COOKIE['remember_token'])) {
-		header('Location: login.php');
-		exit;
+	header('Location: login.php');
+	exit;
 }
 
 echo 'Logged in as user ID: ' . $_SESSION['user_id'];
-
