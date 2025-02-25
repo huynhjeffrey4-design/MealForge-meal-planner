8d2bab859b897eba950ada90bf6d9e5b739415fd
jhuynh34buffalo <jhuynh3@buffalo.edu>
2025-02-25 14:31:53 -0500

    .gitignore config.php
    Add config.php to .gitignore and create example config template

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+   // Get values from .env if available
+   require_once __DIR__ . '/setup.php';
+
+   define('DB_HOST', env('DB_HOST', 'localhost'));
+   define('DB_USER', env('DB_USERNAME', 'username'));
+   define('DB_PASS', env('DB_PASSWORD', 'password'));
+   define('DB_NAME', env('DB_NAME', 'database_name'));
+   ?>
