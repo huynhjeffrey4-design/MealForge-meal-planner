a39c3e171410d32c4617463fe9e261bcdd69e325
Junhua Tang <tangjiaoshou55@gmail.com>
2025-03-27 14:51:16 -0400

    setup connection through redbeanPHP

--- api/get_random_recipes.php
+++ api/get_random_recipes.php
@@ -1,22 +1,28 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../setup.php';
 require_once __DIR__ . '/../SetupRedbean.php';
+require_once __DIR__ . '/../controllers/recipe.php';
+require_once __DIR__ . '/../providers/RandRecipeDataProvider.php';
 
-$sql = "SELECT id, recipe AS meal_name, meal_type, imageURL
-        FROM rand_recipes 
-        ORDER BY RAND() 
-        LIMIT 5";
-        
-$result = $conn->query($sql);
+$provider = new RandRecipeDataProvider();
+$controller = new RecipeController($provider); 
 
-$recipes = [];
+$allRecipes = $controller->getAllRecipes();
+shuffle($allRecipes);
+$randomRecipes = array_slice($allRecipes, 0, 5);
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $recipes[] = $row;
-    }
-}
+$recipes = array_map(function($recipe) {
+    return [
+        'id' => $recipe['id'],
+        'meal_name' => $recipe['recipe'],
+        'meal_type' => $recipe['meal_type'],
+        'imageURL' => $recipe['imageURL'] ?? null
+    ];
+}, $randomRecipes);
 
 header('Content-Type: application/json');
 echo json_encode($recipes);
-?>
