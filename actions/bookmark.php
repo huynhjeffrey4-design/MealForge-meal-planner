a2573da1e8a01b5f5e444a90ecaeb15763046501
Songzhul <songzhul@buffalo.edu>
2025-04-03 20:31:04 -0400

    feat: users can choose meals in weekly plan

--- actions/bookmark.php
+++ actions/bookmark.php
@@ -14,12 +14,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	} else {
 		$recipeId = isset($_POST['recipe_id']) ? intval($_POST['recipe_id']) : null;
 
+		//Get the current date parameter (used to update the corresponding meal plan)
+        $currentDay = $_POST['day'] ?? 'Monday';
+
 		$bookmarkController = new BookmarkController($_SESSION['user']['id']);
+
+		
 
 		$result = $bookmarkController->toggleBookmark($recipeId);
 
 		$recipeController = new RecipeController(null);
 		$recipe = $recipeController->getRecipeById($recipeId);
+		//Update the meal plan in the session based on the toggle result
+		  if ($result['action'] === 'added') {
+            // Remove the recipe from the meal plan for the current date when unliked.
+            $_SESSION['meal_plan'][$currentDay][] = $recipe;
+        } elseif ($result['action'] === 'removed') {
+            // When unliked, remove the recipe from the meal plan of the current date.
+            if (isset($_SESSION['meal_plan'][$currentDay])) {
+                foreach ($_SESSION['meal_plan'][$currentDay] as $index => $r) {
+                    if (isset($r['id']) && $r['id'] == $recipeId) {
+                        array_splice($_SESSION['meal_plan'][$currentDay], $index, 1);
+                        break;
+                    }
+                }
+            }
+        }
+
 
 		$_SESSION['message'] = 'Bookmark toggled for recipe ' . $recipe['recipe'];
 	}
