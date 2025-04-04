bdfc7de57cb65d81bb80644257f1bc2b28ce6484
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-04 18:21:00 -0400

    fix: unescape recipe title, as they are not user submitted values

--- recipe.php
+++ recipe.php
@@ -51,7 +51,7 @@ if (isset($_SESSION['user']) && isset($_SESSION['user']['id']) && $recipe_id) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $recipeNotFound ? 'Recipe Not Found' : htmlspecialchars($recipe['recipe']) ?></title>
+    <title><?= $recipeNotFound ? 'Recipe Not Found' : $recipe['recipe'] ?></title>
     <script src="https://cdn.tailwindcss.com"></script>
 	<script src="https://unpkg.com/lucide@latest"></script>
     <style>
