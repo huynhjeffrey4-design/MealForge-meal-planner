25287a3613d1ee2c831415caf7f013775334dfa2
dsgulvin <dgulvin05@gmail.com>
2025-04-10 00:49:08 -0400

    Comment/spacing fix

--- recipe.php
+++ recipe.php
@@ -180,7 +180,7 @@ handleLikeAction($isLoggedIn, $recipeController, $recipe_id);
 <body class="bg-gray-100">
 <?php include 'header.php'; ?>
     <div class="container mx-auto max-w-3xl p-4">
-        <!-- Return button -->
+        <!-- Back to search button -->
         <div class="mb-6 print-hide">
             <a href="search.php" class="flex items-center text-gray-600 hover:text-gray-900">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2">
